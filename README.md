11ff9cc67db163ccad1dc4e0e2afed54e15931fc
Rahul Kumar <rahulkr1815.rk@gmail.com>
2020-10-28 21:25:34 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,30 @@
+# Project Instructions
+
+The goal of this project are:
+- Setting up Webpack
+- Sass styles
+- Webpack Loaders and Plugins
+- Creating layouts and page design
+- Service workers
+- Using APIs and creating requests to external urls
+
+## Getting started
+
+`cd` into the project folder and run:
+- `npm install`
+
+## Starting the API Server
+
+Run `npm run start` to run the API server
+
+## Starting the Client - Development
+
+Run `npm run build-dev` to run the client
+
+## Starting the Client - Production
+
+Run `npm run build-prod` to run the client
+
+## Run Jest Test
+
+Run `npm run test` to run the tests
---
+++ webpack.dev.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+
+module.exports = {
+  mode: "development",
+  entry: './src/client/index.js',
+  devtool: 'source-map',
+  devServer: {
+    port: 8800
+  },
+  output: {
+    libraryTarget: 'var',
+    library: 'Client',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: '/\.js$/',
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.(png|jpg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]'
+            }
+          }
+        ],
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
+    new CleanWebpackPlugin({
+      dry: true,
+      verbose: true,
+      cleanStaleWebpackAssets: false,
+      protectWebpackAssets: false
+    })
+  ]
+}
